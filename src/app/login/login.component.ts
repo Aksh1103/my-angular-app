d3c32923d0c4a2bc98ae9ba9c45998aa40db3c1f
AkshHeaven <Akone1103@gmail.com>
2024-03-15 09:56:28 -0700

    commit for ts change

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,6 +8,13 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent  {
 
+
+  constructor(private router: Router) { }
+
+
+  navigateToSecond() {
+    this.router.navigate(['/signup']); // Navigate to the second component
+  }
 
 
   
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent {
+
+  
+  constructor(private router: Router) { }
+
+
+  navigateToSecond() {
+    this.router.navigate(['/signup']); // Navigate to the second component
+  }
+
 
 }
